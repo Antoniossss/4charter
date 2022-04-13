72d444d9cf00c6a48bba9a09401f24bc52d010d1
Sebastian Choina <sebollson@gmail.com>
2022-04-13 23:38:01 +0200

    Really last changes, nothing more!

--- src/main/java/charter/customer/CustomerWs.java
+++ src/main/java/charter/customer/CustomerWs.java
@@ -1,38 +1,50 @@
 package charter.customer;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Customer", description = "Customer related CRUD")
+@ApiResponse(responseCode = "404", description = "Customer with given ID was not found")
 @CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 @RestController
-@RequestMapping("customer")
+@RequestMapping(value = "customer",produces = MediaType.APPLICATION_JSON_VALUE)
 public class CustomerWs {
     private final CustomerService customerService;
 
+    @Operation(summary = "List customers", description = "Allows listing of all customers")
     @GetMapping("list")
     public List<Customer> listCustomers() {
         return customerService.listCustomers();
     }
 
+    @Operation(summary = "Create customer", description = "Creates new customer based on request body")
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer) {
         return customerService.saveOrUpdate(customer);
     }
 
+    @Operation(summary = "Update customer", description = "Updates customer based on request body")
     @PutMapping("{customerId}")
     public Customer updateCustomer(@PathVariable("customerId") Long customerId, @RequestBody Customer customer) {
         customer.setId(customerId);
         return customerService.saveOrUpdate(customer);
     }
 
+    @Operation(summary = "Get customer", description = "Gets details of single customer")
     @GetMapping("{customerId}")
     public Customer getCustomer(@PathVariable("customerId") Long customerId) {
         return this.customerService.get(customerId);
     }
 
+    @Operation(summary = "Get customer", description = "Removes given customer. Customer cannot have any purchases - in such case operation will fail")
+    @ApiResponse(responseCode = "412", description = "Given customer has some purchases. Remove them first")
     @DeleteMapping("{customerId}")
     public Customer deleteCustomer(@PathVariable("customerId") Long customerId) {
         return this.customerService.removeCustomer(customerId);
--- src/main/java/charter/purchase/PurchaseWs.java
+++ src/main/java/charter/purchase/PurchaseWs.java
@@ -2,6 +2,10 @@ package charter.purchase;
 
 import charter.points.PointsInMonth;
 import charter.points.PointsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,8 @@ import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 import java.util.List;
 
+@Tag(name = "Purchase", description = "Purchases related CRUD")
+@ApiResponse(responseCode = "404", description = "Purchase with given ID was not found")
 @CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 @RestController
@@ -19,6 +25,7 @@ public class PurchaseWs {
     private final PurchaseService purchaseService;
     private final PointsService pointsService;
 
+    @Operation(summary = "Register purchase", description = "Creates new purchase entry using given parameters")
     @PostMapping
     public Purchase newPurchase(@RequestBody NewPurchaseDto dto) {
         Purchase purchase = new Purchase();
@@ -27,33 +34,44 @@ public class PurchaseWs {
         return purchaseService.createPurchase(dto.customerId, purchase);
     }
 
+    @Operation(summary = "Update purchase", description = "Updates purchase entry using given parameters")
     @PutMapping
     public Purchase updatePurchase(@RequestBody Purchase purchase) {
         return purchaseService.updatePurchase(purchase);
     }
 
+    @Operation(summary = "Get purchase", description = "Gets details of purchase")
     @GetMapping("{id}")
     public Purchase getPurchase(@PathVariable("id") Long id) {
         return purchaseService.getPurchase(id);
     }
 
+    @Operation(summary = "Delete purchase", description = "Removes purchase entry")
     @DeleteMapping("{id}")
     public Purchase deletePurchase(@PathVariable("id") Long id) {
         return purchaseService.deletePurchase(id);
     }
 
+    @Operation(summary = "List purchases", description = "Gets list ot purchases. Optionally can be narrowed do a single customer")
     @GetMapping
-    public List<Purchase> listPurchases(@RequestParam(value = "customerId", required = false) Long id) {
+    public List<Purchase> listPurchases(
+            @Parameter(name = "customerId", description = "ID of customer to filter out purchases", required = false)
+            @RequestParam(value = "customerId", required = false) Long id) {
         if (id == null) {
             return purchaseService.listPurchases();
         }
         return purchaseService.listCustomerPurchases(id);
     }
 
+    @Operation(summary = "Customer points", description = "Allows to retrieve customer points")
+    @ApiResponse(responseCode = "404", description = "Customer with given ID was not found")
     @GetMapping("points")
     public CustomerPointsDto listUserPoints(
+            @Parameter(name = "customerId", required = true, description = "ID of the customer", example = "33")
             @RequestParam(value = "customerId", required = true) Long customerId,
+            @Parameter(name = "from", required = true, description = "'from' date time filter value", example = "2022-01-01T12:00:00Z")
             @RequestParam(value = "from", required = false) Instant from,
+            @Parameter(name = "to", required = true, description = "'to' date time filter value. If missing, fallback to the current timestamp", example = "2022-05-01T12:00:00Z")
             @RequestParam(value = "to", required = false) Instant to) {
         CustomerPointsDto ret = new CustomerPointsDto();
         ret.setTotal(pointsService.calcAllCustomerPoints(customerId));
